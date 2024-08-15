f9e8b9342a8dcabe80a5c34b4f31df633c3c9ae9
Alessandro Cavaliere <alessandrocavaliere41@gmail.com>
2024-08-16 00:59:21 +0200

    fix: :bug: Fix some bugs on `DeployByteTheCookiesNFT.s.sol` and integrate it with `HelperConfig.s.sol`

---
+++ script/DeployByteTheCookiesNFT.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {ByteTheCookiesNFTCollection} from "../src/ByteTheCookiesNFTCollection.sol";
+contract DeployByteTheCookiesNFT is Script{
+
+    /*//////////////////////////////////////////////////////////////
+                             STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+    address public player = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; // Base Account of Local Chain Anvil
+
+    /// @notice Deploy the ByteTheCookiesNFTCollection contract
+    /// @dev Deploy the ByteTheCookiesNFTCollection contract with the network configuration
+    /// @return ByteTheCookiesNFTCollection The deployed ByteTheCookiesNFTCollection contract
+    function run()  external returns (ByteTheCookiesNFTCollection, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        vm.startBroadcast();
+        ByteTheCookiesNFTCollection nftContract= new ByteTheCookiesNFTCollection(
+            config.name,
+            config.symbol,
+            config.owner
+        );
+        vm.stopBroadcast();
+        return (nftContract, helperConfig);
+    }
+}
